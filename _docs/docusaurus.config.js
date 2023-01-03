a4d04e7efe0717228aeea08ea3c3a7f86e01b566
kalevski <dakalevski@gmail.com>
2023-01-03 18:56:18 +0100

    removed yarn cache

--- _docs/docusaurus.config.js
+++ _docs/docusaurus.config.js
@@ -19,6 +19,7 @@ const config = {
   organizationName: 'kalevski', // Usually your GitHub org/user name.
   projectName: 'phaser-plus', // Usually your repo name.
   deploymentBranch: 'public',
+  trailingSlash: false,
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
