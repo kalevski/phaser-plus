df5c26c86170d66c87d0d70b7e7382a71cb5c3f7
kalevski <dakalevski@gmail.com>
2023-01-25 12:42:48 +0100

    HTMLFeature latest changes

--- core/src/HTMLFeature.js
+++ core/src/HTMLFeature.js
@@ -54,6 +54,7 @@ HTMLFeature.setup = () => {
         width: 100%;
         height: 100%;
         overflow: hidden;
+        position: absolute;
     }
     .html-feature * {
         pointer-events: auto;
